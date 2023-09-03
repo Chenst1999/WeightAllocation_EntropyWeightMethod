839ce918f6e2472feafec009a2651bb2d0c7a419
Chenst <69191997+Chenst1999@users.noreply.github.com>
2023-09-03 13:16:42 +0800

    Add files via upload

---
+++ main.m
@@ -0,0 +1,16 @@
+clc;
+load entropy_datas  % load the data
+
+%加载数据  列数表示指标数 ， 行数表示评价的个体数 
+%此数据 7个评价个体 3个评价指标
+X = shang_datas 
+
+%说明指标是正向指标还是负向指标
+%此数据第一个是负向指标， 其余为正向指标
+Ind=[2 1 1]; %Specify the positive or negative direction of each indicator
+
+%S 为分数排名 W为指标权重
+[S,W]=entropy(X,Ind) % get the score
+
+
+
